e4b5f2c0320793a170e15eaa8d572e1a1b1515d6
Asikur Rahaman <mdasikur5893@gmail.com>
2026-05-03 11:01:41 +0600

    project setup and some extentation install

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+
+const App = () => {
+  return (
+    <div>
+      <h3>hi</h3>
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default App;
